d208fe6f8d416428b107acde3cbe8decdde6dede
Matheus Alves <matheusalves.bmd@gmail.com>
2023-02-14 20:10:29 -0300

    Convert the ages and calculate the average

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const dataKate = [
   [10, 5, 6, 1, 4],
 ];
 
+const dogsAges = [
+  [5, 2, 4, 1, 15, 8, 3],
+  [16, 6, 10, 5, 6, 1, 4],
+];
+
 const checkDogs = (dogsJulia, dogsKate) => {
   const dogsOnlyJulia = dogsJulia.slice(1, -2);
 
@@ -15,6 +20,14 @@ const checkDogs = (dogsJulia, dogsKate) => {
   dogsAgeAll.forEach((age, idx) => (age < 3 ? console.log(`Dog number ${idx + 1} is still a puppy 🐶`) : console.log(`Dog number ${idx + 1} is an adult, and is ${age} years old`)));
 };
 
-checkDogs(dataJulia[0], dataKate[0]);
-console.log("\n");
-checkDogs(dataJulia[1], dataKate[1]);
+const calcAverageHumanAge = ages => {
+  const humanAges = ages.map(age => (age <= 2 ? age * 2 : 16 + age * 4)).filter(age => age >= 18);
+  return humanAges.reduce((acc, age) => acc + age, 0) / humanAges.length;
+};
+
+// checkDogs(dataJulia[0], dataKate[0]);
+// console.log("\n");
+// checkDogs(dataJulia[1], dataKate[1]);
+
+console.log(calcAverageHumanAge(dogsAges[0]));
+console.log(calcAverageHumanAge(dogsAges[1]));
